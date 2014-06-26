c0e821a13a65566c3ce8a1394a91ea0b2730b981
Adrian <bishop@localhost.localdomain>
2014-06-26 09:32:42 -0400

    added utils to apt-get

--- apt-install.sh
+++ apt-install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# Install packages
+# Update packages
 sudo apt-get -y update
 sudo apt-get -y upgrade
 # Basic utilities
-sudo apt-get -y install vim emacs screen byobu scons make unzip rlwrap
+sudo apt-get -y install coreutils
+sudo apt-get -y install screen byobu scons make unzip rlwrap
 # C++ build libraries
 sudo apt-get -y install g++ libboost-all-dev libevent-dev openssl gdb
 sudo apt-get -y install libncurses5-dev libsqlite3-dev
@@ -12,7 +13,6 @@ sudo apt-get -y install build-essential g++ gcc binutils make clang
 sudo apt-get -y install libclang-common-dev libclang-dev libclang1
 sudo apt-get -y install libllvm-ocaml-dev libllvm3.1 
 sudo apt-get -y install llvm llvm-dev 
-
 # Parsing utilities
 sudo apt-get -y install lemon flex bison
 # Java packages
@@ -28,9 +28,16 @@ sudo apt-get -y install libfreetype6-dev libpng-dev libblas-dev liblapack-dev
 # monitoring utilities
 sudo apt-get -y install collectl tcpdump
 sudo apt-get -y install wireshark
-# misc
+# Editors
+sudo apt-get -y install gedit geany emacs vim geany
+# Security
+udo apt-get -y install nmap
+# Misc
 sudo apt-get -y install coreutils
 sudo apt-get -y install curl
 sudo apt-get -y install openssh-server openssh-client
 sudo apt-get -y install ghex hexedit
 sudo apt-get -y install gedit
+sudo apt-get -y install chromium
+sudo apt-get -y install vlc
+sudo apt-get -y install wine
