51d03b022970b751b2f59f0d48b3a2c46e1a2dfd
ad2883 <none@none.com>
2017-11-29 13:09:42 -0500

    added alias for vim

--- dot_files/.bash_aliases
+++ dot_files/.bash_aliases
@@ -7,6 +7,8 @@ alias lsdir="ls -aF | grep '/$'"
 alias h=history
 alias rmtilde=' set nonomatch ; rm -f core .*~ *~ \#*; unset nonomatch ;'
 
+# vim alias
+alias vi='vim'
 
 # Makes sure that sqlite3 doesn't have any readline issues, and allows usage of
 # arrow keys for history retrieval
---
+++ python-install.sh
@@ -0,0 +1,11 @@
+# Python and Python modules
+
+# Python 2
+# Note: matplot is installed via the apt-install script
+python -m pip --proxy $http_proxy install --upgrade pip
+python -m pip --no-cache-dir --proxy $http_proxy install --user -r python2-requirements.txt
+
+# Python 3
+python3 -m pip --proxy $http_proxy install --upgrade pip
+python3 -m pip --no-cache-dir --proxy $http_proxy install --user -r python3-requirements.txt
+
