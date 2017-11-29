6c3c350fe06d4b63f92f89e8440e73546ee5e3e3
ad2883 <ad23883@ad23883-debian.llan.ll.mit.edu>
2017-11-29 13:07:47 -0500

    added python3 and updated version numbers of some packages

--- apt-install.sh
+++ apt-install.sh
@@ -11,20 +11,22 @@ sudo apt-get -y install g++ libboost-all-dev libevent-dev openssl gdb
 sudo apt-get -y install libncurses5-dev libsqlite3-dev
 sudo apt-get -y install build-essential g++ gcc binutils make clang
 sudo apt-get -y install libclang-common-dev libclang-dev libclang1
-sudo apt-get -y install libllvm-ocaml-dev libllvm3.1 
+sudo apt-get -y install libllvm-ocaml-dev libllvm3.9 
 sudo apt-get -y install llvm llvm-dev 
 # Parsing utilities
 sudo apt-get -y install lemon flex bison
 # Java packages
-sudo apt-get -y install openjdk-6-jdk openjdk-6-jre 
+sudo apt-get -y install openjdk-8-jdk openjdk-8-jre 
 # Version control utilities
 sudo apt-get -y install libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev
 sudo apt-get -y install git-all
 sudo apt-get -y install bash-completion
 # Python package management utilities
-sudo apt-get -y install python-pip python-virtualenv virtualenvwrapper 
+sudo apt-get -y install python3
+sudo apt-get -y install python-pip python3-pip python-virtualenv virtualenvwrapper 
 # numpy and matplotlib dependencies
 sudo apt-get -y install libfreetype6-dev libpng-dev libblas-dev liblapack-dev
+sudo apt-get build-dep python-matplotlib
 # monitoring utilities
 sudo apt-get -y install collectl tcpdump
 sudo apt-get -y install wireshark
