46b0539ccdd07773ecda8cf5154a33bb1a937e8b
analyst <analyst@elk-debain.test_bed>
2018-09-30 09:31:33 -0400

    added space before ] in if statements

--- dot-file-setup.sh
+++ dot-file-setup.sh
@@ -5,23 +5,23 @@ if [ -f ~/.bash_aliases ]; then
   cp ~/.bash_aliases .bash_aliases.bck
 fi
 cp dot_files/.bash_aliases ~/
-if [ -f ~/.bash_logout]; then
+if [ -f ~/.bash_logout ]; then
   cp ~/.bash_logout ~/.bash_logout.bck
 fi
 cp dot_files/.bash_logout ~/
-if [ -f ~/.bashrc]; then
+if [ -f ~/.bashrc ]; then
   cp ~/.bashrc ~/.bashrc.bck
 fi
 cp dot_files/.bashrc ~/
-if [ -f ~/.profile]; then
+if [ -f ~/.profile ]; then
   cp ~/.profile ~/.profile.bck
 fi
 cp dot_files/.profile ~/
-if [ -f ~/.screenrc]; then
+if [ -f ~/.screenrc ]; then
   cp ~/.screenrc ~/.screenrc.bck
 fi
 cp dot_files/.screenrc ~/
-if [ -f ~/.vimrc]; then
+if [ -f ~/.vimrc ]; then
   cp ~/.vimrc ~/.vimrc.bck
 fi
 cp dot_files/.vimrc ~/
